db1a1fe9141e0032620b319f6f89b6407944c604
KalebBrommerich <98110716+KalebBrommerich@users.noreply.github.com>
2025-07-06 13:51:53 -0500

    Added pictures for factions and got carousel working

--- BasicDotNetMVC/Controllers/HomeController.cs
+++ BasicDotNetMVC/Controllers/HomeController.cs
@@ -15,7 +15,28 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var slides = new List<HeadlineParagraphImageModel>
+    {
+        new HeadlineParagraphImageModel
+        {
+            Headline = "",
+            Description = "The ravenous Terminids are guided solely by an instinctive urge to reproduce, spread, and destroy Democracy. They are a fast-evolving plague, a shapeshifting infestation that threatens to devour and disenfranchise every single child on every single planet.",
+            BackgroundImageUrl = "/Images/Terminid.png"
+        },
+        new HeadlineParagraphImageModel
+        {
+            Headline = "",
+            Description = "The Automatons are mindless, bloodthirsty robots, coded for nothing but murder and socialist violence. Their origins are a mystery, but their unthinking hatred of Freedom makes them a threat to all citizens of Super Earth.",
+            BackgroundImageUrl = "/Images/Automaton.png"
+        },
+        new HeadlineParagraphImageModel
+        {
+            Headline = "",
+            Description = "An ancient, “highly advanced” species, desperately seeking to reclaim their former galactic supremacy. These mind-controlling, xenophobic aliens cannot be reasoned with.",
+            BackgroundImageUrl = "/Images/Illuminate.png"
+        }
+    };
+        return View(slides);
     }
 
     public IActionResult Privacy()
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Themed around being a Super Earth enlistment website from the "Helldivers" unive
 # Image Credits
 Helldivers logo is from the game (Arrowhead Development)
 Super Earth Enlistment is from this website https://www.pcgamer.com/games/third-person-shooter/farewell-battle-brother-helldivers-2-players-honour-a-late-soldier-with-thousands-of-salutes-arrowhead-games-ceo-says-hell-work-with-the-dev-team-on-a-fitting-tribute/ (Arrowhead Devlopment)
+Illuminate and Terminid pictures are from the Helldivers Wiki (https://helldivers.wiki.gg/wiki/Illuminate https://helldivers.wiki.gg/wiki/Terminids)
+Automaton picute is from an n4g article (https://n4g.com/articles/new-helldivers-2-major-order-sees-the-automatons-help-out-the-terminids/)
